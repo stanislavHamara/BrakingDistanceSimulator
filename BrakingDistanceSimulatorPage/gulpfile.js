88d05b5d2b7904f60bd26990d3c1549b52526f32
stan <stanislav.hamara@gmail.com>
2015-10-01 11:36:18 +0100

    finish gulp setup

--- BrakingDistanceSimulatorPage/gulpfile.js
+++ BrakingDistanceSimulatorPage/gulpfile.js
@@ -5,10 +5,37 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-minify-css');
 var rename = require("gulp-rename");
-var sass = require('gulp-sass');
+var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 
-//default task
-gulp.task('default', function(){
+gulp.task('default', ['scripts', 'styles']);
 
+// scripts task
+gulp.task('scripts', function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist/js/'));
+});
+
+// styles task
+gulp.task('styles', function() {
+    return gulp.src('./src/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist/css/'));
+});
+
+// watch task
+gulp.task('watch', function() {
+    gulp.watch('./src/js/*.js', ['scripts']);
+    gulp.watch('./src/less/*.less', ['styles']);
 });
