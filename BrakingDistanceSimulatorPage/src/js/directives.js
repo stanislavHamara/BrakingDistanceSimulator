c56349db7313d74a19b286a1adb3f3a5181240a5
stan <stanislav.hamara@gmail.com>
2015-10-13 15:01:06 +0100

    create a directive for control panel

--- BrakingDistanceSimulatorPage/src/js/directives.js
+++ BrakingDistanceSimulatorPage/src/js/directives.js
@@ -1,7 +1,14 @@
-angular.module('simulator.directives',[])
-    .directive("bdsMenu", function(){
-        return{
+angular.module('simulator.directives', [])
+    .directive("bdsMenu", function () {
+        return {
             restrict: 'E',
             templateUrl: './dist/templates/bds-menu.html'
+        }
+    })
+
+    .directive('bdsControlPanel', function () {
+        return {
+            restrict: 'E',
+            templateUrl: './dist/templates/bds-control-panel.html'
         }
     });
