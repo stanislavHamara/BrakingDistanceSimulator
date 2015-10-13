e47c47d18fa9cdc68da243255c92de8611880f65
stan <stanislav.hamara@gmail.com>
2015-10-13 15:01:25 +0100

    extend the controller to manage all the controls

--- BrakingDistanceSimulatorPage/src/js/app.js
+++ BrakingDistanceSimulatorPage/src/js/app.js
@@ -1,5 +1,2 @@
 angular.module('simulator', ['simulator.directives','simulator.controllers', 'simulator.services'])
-    .config(function(){
-        $("[name='my-checkbox']").bootstrapSwitch();
-    });
 
--- BrakingDistanceSimulatorPage/src/js/controllers.js
+++ BrakingDistanceSimulatorPage/src/js/controllers.js
@@ -1,5 +1,10 @@
 angular.module('simulator.controllers', [])
-    .controller('NavController', ['$scope','Properties', function($scope, Properties){
+    .controller('PropertiesController', ['$scope', 'Properties', function ($scope, Properties) {
+
+        $("[name='my-checkbox']").bootstrapSwitch();
+
         $scope.surfaces = Properties.getSurfaces();
         $scope.weather = Properties.getWeather();
+
+        $scope.speedButtons = ['-1','-5','-10','+10','+5','+1'];
     }]);
