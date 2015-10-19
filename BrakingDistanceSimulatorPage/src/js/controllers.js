1e117c5ad014372a2f440aaabc47646af389ed53
stan <stanislav.hamara@gmail.com>
2015-10-19 11:58:56 +0100

    resize the scene on window resize

--- BrakingDistanceSimulatorPage/src/js/controllers.js
+++ BrakingDistanceSimulatorPage/src/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('simulator.controllers', [])
+angular.module('simulator.controllers', ['rt.resize'])
     .controller('PropertiesController', ['$scope', 'Properties', function ($scope, Properties) {
 
         $scope.surfaces = Properties.getSurfaces();
@@ -47,45 +47,53 @@ angular.module('simulator.controllers', [])
         }
     }])
 
-    .controller('CanvasController',['$scope', function($scope){
+    .controller('CanvasController', ['$scope', 'resize', function ($scope, resize) {
         $scope.element = document.getElementById('bds-threejs-container');
 
         var camera, scene, renderer;
         var geometry, material, mesh;
 
+        $scope.initScene = function () {
 
-        $scope.init = function() {
-
-            camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
             camera.position.z = 1000;
 
             scene = new THREE.Scene();
 
-            geometry = new THREE.BoxGeometry( 200, 200, 200 );
-            material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true} );
+            geometry = new THREE.BoxGeometry(200, 200, 200);
+            material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
 
-            mesh = new THREE.Mesh( geometry, material );
-            scene.add( mesh );
+            mesh = new THREE.Mesh(geometry, material);
+            scene.add(mesh);
 
             renderer = new THREE.WebGLRenderer();
-            renderer.setSize( $scope.element.offsetWidth, $scope.element.offsetHeight );
+            renderer.setSize($scope.element.offsetWidth, $scope.element.offsetHeight);
 
-            $scope.element.appendChild( renderer.domElement );
+            $scope.element.appendChild(renderer.domElement);
 
         };
 
-        $scope.animate = function() {
+        $scope.animate = function () {
 
             // note: three.js includes requestAnimationFrame shim
-            requestAnimationFrame( $scope.animate );
+            requestAnimationFrame($scope.animate);
 
             mesh.rotation.x += 0.01;
             mesh.rotation.y += 0.02;
 
-            renderer.render( scene, camera );
+            renderer.render(scene, camera);
         };
 
-        $scope.init();
+        resize($scope).call(function () {
+
+            camera.aspect = $scope.element.offsetWidth / $scope.element.offsetHeight;
+            camera.updateProjectionMatrix();
+
+            renderer.setSize($scope.element.offsetWidth, $scope.element.offsetHeight);
+
+        });
+
+        $scope.initScene();
         $scope.animate();
 
     }]);
