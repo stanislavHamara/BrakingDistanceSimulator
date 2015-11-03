180010bbe70f3ba8f8dd4154aa778ee3311d3693
stan <stanislav.hamara@gmail.com>
2015-11-03 14:59:51 +0000

    calculate braking distance

--- BrakingDistanceSimulatorPage/src/js/Services/PhysService.js
+++ BrakingDistanceSimulatorPage/src/js/Services/PhysService.js
@@ -4,8 +4,12 @@ angular.module('PhysicsService', [])
         var g = 9.81; //standard gravity value
         var v, cf; //velocity and coefficient of friction
 
-        var getThinkingDistance = function(v){
-            return  v * 3 / 10; //m;
+        var getThinkingDistance = function (v) {
+            return v * 3 / 10; //m;
+        };
+
+        var getBrakingDistance = function (v, cf) {
+            return Math.pow(v * 0.45, 2) / (2 * cf * g);
         };
 
         return {
@@ -13,7 +17,7 @@ angular.module('PhysicsService', [])
                 var v = (userInput.imperial ? userInput.speed : userInput.speed / 1.6); //mph
                 return {
                     thinkingDistance: getThinkingDistance(v),
-                    brakingDistance: 0
+                    brakingDistance: getBrakingDistance(v, 0.7) // coefficient of friction used for dry asphalt
                 };
             }
         };
