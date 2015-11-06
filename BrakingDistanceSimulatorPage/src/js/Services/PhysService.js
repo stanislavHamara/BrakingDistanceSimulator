44125b07e814ac824ee503e60fe6c373cedb7744
stan <stanislav.hamara@gmail.com>
2015-11-06 11:26:45 +0000

    get coefficient from the method instead of being hard coded

--- BrakingDistanceSimulatorPage/src/js/Services/PhysService.js
+++ BrakingDistanceSimulatorPage/src/js/Services/PhysService.js
@@ -4,20 +4,25 @@ angular.module('PhysicsService', [])
         var g = 9.81; //standard gravity value
         var v, cf; //velocity and coefficient of friction
 
+        var getCoefficient = function(surface, condition){
+            return 0.7;
+        };
+
         var getThinkingDistance = function (v) {
             return v * 3 / 10; //m;
         };
 
         var getBrakingDistance = function (v, cf) {
-            return Math.pow(v * 0.45, 2) / (2 * cf * g);
+            return Math.pow(v * 0.45, 2) / (2 * cf * g); //m
         };
 
         return {
             getStoppingDistance: function (userInput) {
-                var v = (userInput.imperial ? userInput.speed : userInput.speed / 1.6); //mph
+                v = (userInput.imperial ? userInput.speed : userInput.speed / 1.6); //mph
+                cf = getCoefficient(userInput.surface, userInput.condition);
                 return {
                     thinkingDistance: getThinkingDistance(v),
-                    brakingDistance: getBrakingDistance(v, 0.7) // coefficient of friction used for dry asphalt
+                    brakingDistance: getBrakingDistance(v, cf) // coefficient of friction used for dry asphalt
                 };
             }
         };
