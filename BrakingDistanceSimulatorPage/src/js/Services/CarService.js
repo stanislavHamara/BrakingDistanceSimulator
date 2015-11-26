8670b615fb83c052857932c0a30cb4ead2dda687
stan <stanislav.hamara@gmail.com>
2015-11-26 15:56:09 +0000

    import car using Car.js

--- BrakingDistanceSimulatorPage/src/js/Services/CarService.js
+++ BrakingDistanceSimulatorPage/src/js/Services/CarService.js
@@ -1,66 +1,31 @@
 angular.module('CarService', [])
     .factory('CarService', function () {
-        var mesh;
-        var instance;
-        var carGroup = new THREE.Object3D();
+        var car;
 
-        function JSONLoad(scene, url, id, camera) {
-            var loader = new THREE.JSONLoader();
-
-            loader.load(url, function (geometry, materials) {
-                mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
-                loader.onLoadComplete(materials);
-            });
-
-            loader.onLoadComplete = function (materials) {
-                mesh.scale.set(0.1, 0.1, 0.1);
-                mesh.overdraw = true;
-                mesh.castShadow = true;
-                mesh.position.set(0, 0, -100);
-                for (var i = 0; i < materials.length; i++) {
-                    materials[i].side = THREE.DoubleSide;
-                }
-
-                if (id == 1) {
-                    carGroup.add(mesh);
-                    camera.target = mesh;
-                } else {
-                    addWheels(mesh);
-                }
-
-                if (carGroup.children.length == 5) {
-                    scene.add(carGroup);
-                    carGroup.rotation.y = 1;
-                    carGroup.position.set(70,0,0);
-                    console.log(carGroup);
-                }
+        function loadCar(scene, camera) {
+            car = new THREE.Car();
+            car.modelScale = 0.1;
+            car.backWheelOffset = 60;
+            car.loadPartsJSON("dist/js/models/body.js", "dist/js/models/wheel.js");
+            car.callback = function (object) {
+                addCar(object, 0, 0, 0, scene, camera);
             }
         }
 
-        function addWheels(mesh) {
-            for (var j = 0; j < 4; j++) {
-                instance = mesh.clone();
-                switch (j) {
-                    case 1:
-                        instance.position.set(285, 0, -100);
-                        break;
-                    case 2:
-                        instance.position.set(-20, 0, -100);
-                        instance.rotation.y = Math.PI;
-                        break;
-                    case 3:
-                        instance.position.set(-304, 0, -100);
-                        instance.rotation.y = Math.PI;
-                        break;
-                }
-                carGroup.add(instance);
-            }
+        function addCar(object, x, y, z, scene, camera){
+
+            object.root.position.set( x, y, z );
+            object.enableShadows(true);
+
+            camera.target = object;
+
+            scene.add( object.root );
+            console.log(object);
         }
 
         return {
             getCar: function (scene, camera) {
-                JSONLoad(scene, "dist/js/models/body.js", 1, camera);
-                JSONLoad(scene, "dist/js/models/wheel.js", 2);
+                loadCar(scene, camera);
             }
         }
     });
