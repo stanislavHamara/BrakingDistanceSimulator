169dc4fba1a3095511d936d5d2e5dee940ae79db
stan <stanislav.hamara@gmail.com>
2016-02-09 13:53:28 +0000

    set button selection for cameras

--- BrakingDistanceSimulatorPage/src/js/Controllers/CameraController.js
+++ BrakingDistanceSimulatorPage/src/js/Controllers/CameraController.js
@@ -1,4 +1,12 @@
 angular.module('Camera', [])
     .controller('CameraController', ['$scope', function ($scope) {
-            
-        }]);
+        $scope.currentCameraIndex = 1;
+
+        $scope.setCamera = function (cameraIndex) {
+            $scope.currentCameraIndex = cameraIndex;
+        };
+
+        $scope.checkSelectedCamera = function (cameraIndex) {
+            return cameraIndex == $scope.currentCameraIndex;
+        };
+    }]);
